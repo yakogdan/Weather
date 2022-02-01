18b342cdfde9f9a62130b46b845b67d728e4f0e0
Bogdan <yakogdan@gmail.com>
2022-02-02 01:17:37 +0800

    Сделаны временные наполнения фрагментов c погодой

--- app/src/main/java/com/yakogdan/weather/ui/threehours/ThreeHoursFragment.kt
+++ app/src/main/java/com/yakogdan/weather/ui/threehours/ThreeHoursFragment.kt
@@ -28,10 +28,10 @@ class ThreeHoursFragment : Fragment() {
         _binding = FragmentThreeHoursBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textDashboard
+//        dashboardViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
